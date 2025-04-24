5aaa4fd3da0db095c999fd3060bb86c568b79096
Flyte-less <83427235+Flyte-less@users.noreply.github.com>
2025-04-24 09:30:09 -0700

    remove ae2 budding quartz recipes

--- kubejs/server_scripts/key_mods/ae2.js
+++ kubejs/server_scripts/key_mods/ae2.js
@@ -1,4 +1,8 @@
 // priority: 1
 ServerEvents.recipes(event => {
     event.remove({ output: "ae2:vibration_chamber" })
+
+    event.remove({ id: "ae2:transform/flawed_budding_quartz" })
+    event.remove({ id: "ae2:transform/chipped_budding_quartz" })
+    event.remove({ id: "ae2:transform/damaged_budding_quartz" })
 })
