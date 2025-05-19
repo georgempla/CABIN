881e4a1b3be30ede7e4a1046998b2edfda4493af
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2025-05-18 22:21:29 -0500

    1.20.1 create addons (#265)
    * 4 AddOns returned
    
    Create Encased, Create Connected, Create Diesel Generators, and Create Crafts & Additions readded using 1.18 compatibility patches edited to work with 1.20.1
    
    * Fix lint errors
    
    * C&A compat tweaks
    
    * Improve Create Connected compatiblity script
    
    * next patch
    
    ---------
    
    Co-authored-by: kaspertfg <102767907+kaspertfg@users.noreply.github.com>
    Co-authored-by: cuberyl-catastrophe <150861277+cuberyl-catastrophe@users.noreply.github.com>
    Co-authored-by: CubeBotFan <150861277+cubebotfan@users.noreply.github.com>

--- kubejs/server_scripts/server_compatability/create_connected.js
+++ kubejs/server_scripts/server_compatability/create_connected.js
@@ -1,7 +1,15 @@
 if(Platform.isLoaded("create_connected")) {
     ServerEvents.recipes(event => {
         event.remove({ id: "create_connected:sequenced_assembly/control_chip" })
+        event.remove({ id: "create_connected:crafting/kinetics/sequenced_pulse_generator" })
+        event.shapeless("create_connected:sequenced_pulse_generator", ["projectred_core:platformed_plate", "create:electron_tube"])
+            .id("kubejs:compat/create_connected/sequenced_pulse_generator_manual_only")
+        event.recipes.create.deploying("create_connected:sequenced_pulse_generator", ["projectred_core:platformed_plate", "create:electron_tube"])
+    })
 
-        createMachine("projectred_core:platformed_plate", event, "create_connected:sequenced_pulse_generator", "create:electron_tube")
+    ServerEvents.tags("item", event => {
+        event.get("kubejs:sellable_discs")
+            .add("create_connected:music_disk_elevator")
+            .add("create_connected:music_disk_interlude")
     })
 }
